95a864f7096e555f5aae4956ec04860bcc41e1a2
eejneb <93733208+ima-wuman@users.noreply.github.com>
2021-12-06 06:46:11 +0000

    started adding choices

---
+++ main.py
@@ -0,0 +1,15 @@
+print ('''vote for one of these
+1. Machine Learning
+2. blockchain
+3. Advanced Web development
+4. UI design''')
+
+def vote(x):
+  Machine_Learning = 0
+  blockchain = 0
+  Advanced_Web_development = 0
+  UI_design = 0
+  if x == 1:
+    Machine_Learning = Machine_Learning + 1
+    print (Machine_Learning) 
+
