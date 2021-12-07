bd5398fac00a3343acadcc22e4badb4ae77e22fa
ima-wuman <93733208+ima-wuman@users.noreply.github.com>
2021-12-06 21:27:54 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,38 @@
-print ('''vote for one of these
+print ('''vote for one of these:
 1. Machine Learning
-2. blockchain
+2. Blockchain
 3. Advanced Web development
-4. UI design''')
+4. UI design
+''')
 
 def vote(x):
   Machine_Learning = 0
-  blockchain = 0
+  Blockchain = 0
   Advanced_Web_development = 0
   UI_design = 0
-  if x == 1:
+  if x == '1':
     Machine_Learning = Machine_Learning + 1
-    print (Machine_Learning) 
+    print ('you voted for Machine Learning')
+  elif x == '2':
+    Blockchain = Blockchain + 1
+    print ('you voted for Blockchain')
+  elif x == '3':
+    Advanced_Web_development = Advanced_Web_development + 1
+    print ('you voted for Advanced Web development')
+  elif x == '4': 
+    UI_design = UI_design + 1
+    print ('you voted for UI design')
+  else:
+    print('choose the responding number')
+    vote(input())
+    return 
+  print ('''
+votes''')
+  print ('Machine Learning ' + str(Machine_Learning))
+  print ('Blockchain ' + str(Blockchain))
+  print ('Advanced Web development ' + str(Advanced_Web_development))
+  print ('UI design ' + str(UI_design))
 
+  
+
+vote(input())
